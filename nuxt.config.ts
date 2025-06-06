37ff06302ba7b07eae82276ba3a623670a2ff345
nardjo <jord.bastin@gmail.com>
2025-06-06 15:37:11 +0200

    chore: update dependencies and improve robots.txt location

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  css: ['~/assets/css/main.css'],
+  
+  modules: ['@nuxt/ui-pro'],
+
+  
 })
